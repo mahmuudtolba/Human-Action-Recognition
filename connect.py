ad4be4a193149d8b865d05d129175b52036e2259
mahmuudtolba <121031950+mahmuudtolba@users.noreply.github.com>
2023-04-27 08:30:54 +0200

    Add files via upload

--- connect.py
+++ connect.py
@@ -13,6 +13,8 @@ import json
 from cvzone.FaceMeshModule import FaceMeshDetector
 import pyttsx3
 import cvzone
+from keras.utils import img_to_array
+from keras.preprocessing import image
 
 # Set mediapipe model 
 def make_prediction(URL):
@@ -22,7 +24,9 @@ def make_prediction(URL):
     sequence = []
     predictions = []
     threshold = 0.99
+    d = 0
     st = time.time()
+    order = ['get ready' , 'open the page' , 'put the book in the bag']
 
     with mp_holistic_model.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
         while cap.isOpened() :
@@ -53,14 +57,29 @@ def make_prediction(URL):
                     engine.runAndWait()
 
                 elif d < 100 :
-                    engine.say("Please move back")
+                    engine.say("Please move back !")
                     engine.runAndWait()
 
                 else:
-                    """
-                    PUT YOUR CODE HERE 'image' IS THE FRAME TO INSERT TO THE MODEL
-                    """
-                    # Make detections
+                    # Expression code
+                    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+                    faces = face_classifier.detectMultiScale(gray)
+
+                    for (x,y,w,h) in faces:
+                        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
+                        roi_gray = gray[y:y+h,x:x+w]
+                        roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
+                            
+                    if np.sum([roi_gray])!=0:
+                        roi = roi_gray.astype('float')/255.0
+                        roi = img_to_array(roi)
+                        roi = np.expand_dims(roi,axis=0)
+
+                        prediction_expression = expression_model.predict(roi)[0]
+                        label=emotion_labels[prediction_expression.argmax()]
+                                                
+                    # Action code
+                    
                     image, results = mediapipe_detection(frame, holistic)
                     
                     # Draw landmarks
@@ -68,11 +87,12 @@ def make_prediction(URL):
                     
                     # 2. Prediction logic
                     keypoints = extract_keypoints(results)
+                    
                     sequence.append(keypoints)
                     sequence = sequence[-50:]
                     
                     if len(sequence) == 50:
-                        res = model.predict(np.expand_dims(sequence, axis=0))[0]
+                        res = action_model.predict(np.expand_dims(sequence, axis=0))[0]
                         predictions.append(np.argmax(res))
                         
                         
@@ -82,33 +102,40 @@ def make_prediction(URL):
                                 sequence = []
                                 time.sleep(1)
                                 
-                                prediction = actions[np.argmax(res)] # open - put - stand
-                                print(prediction)
-                                r.put(f'https://flutter-app-79e87-default-rtdb.firebaseio.com/{prediction}.json', json.dumps({'status' : 'Done' }))
+                                action_prediction = actions[np.argmax(res)] # open - put - stand
+                                print(action_prediction) 
+                                # URL to send action
+                                r.put(f'https://flutter-app-79e87-default-rtdb.firebaseio.com/{action_prediction}.json', json.dumps({'status' : 'Done' }))
+                                r.put(f'https://flutter-app-79e87-default-rtdb.firebaseio.com/status.json', json.dumps({'feel' : label}))
+                                order.pop(0) 
+                                
+
                     
                     # Show to screen
-                    cv2.imshow('OpenCV Feed', image)
+                    cv2.imshow('OpenCV Feed', frame)
                     et = time.time()
                     # Break gracefully
-                    if ( cv2.waitKey(10) & 0xFF == ord('q') ) or (et - st > 30) :
+                    if ( cv2.waitKey(10) & 0xFF == ord('q') ) or (et - st > 60) or len(order) == 0:
                         r.put(URL ,json.dumps('stop'))
+                        cap.release()
+                        cv2.destroyAllWindows()
                         return 
             else :
-                engine.say("Can't see a person , waiting two seconds")
+                engine.say("I can't see clearly. Please stand in good lighting !")
                 engine.runAndWait()
                 sequence = []
                 predictions = []
-                time.sleep(2) # i don't like you
-        cap.release()
-        cv2.destroyAllWindows()
+                time.sleep(2)
+        
 
 
 if __name__ == '__main__':
-    actions = np.array(['open' , 'put' , 'stand' ])
+    actions = np.array(['open' , 'put' , 'ready'])
+    emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']
     label_map = {label:num for num , label in enumerate(actions)}
 
     # Loading models
-    model = keras.models.load_model('action.h5')
+    action_model = keras.models.load_model('action.h5')
     mp_holistic_model = mp.solutions.holistic
     mp_drawing = mp.solutions.drawing_utils 
 
@@ -117,14 +144,17 @@ if __name__ == '__main__':
     # Setting speaker settings
     engine = pyttsx3.init()
     detector = FaceMeshDetector(maxFaces=1)
-    d = 0
+    expression_model = keras.models.load_model("D:\graduation_project\model_expression.h5")
+    face_classifier = cv2.CascadeClassifier("D:\graduation_project\haarcascade_frontalface_default.xml")
+    
+    
 
     # URL that control running program
     URL  = 'https://flutter-app-79e87-default-rtdb.firebaseio.com/tasks/task1.json'
-
+  
     while True:
         if r.get(URL).json() == 'run':
             make_prediction(URL)
         else:
             print('Program stoped , Run it !')
-        time.sleep(2) # number of seconds waiting
+        time.sleep(3) # number of seconds waiting for action [run or stop]
